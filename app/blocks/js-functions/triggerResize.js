0648d4d52ac21685dcb56ff1d9a09eb6c0035e4b
Allkrain <aukra25@yandex.ru>
2021-03-08 00:50:59 +0300

    Update 2021-03-07T21:50:01.854Z

--- app/blocks/js-functions/triggerResize.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-export function triggerResize () {
-  var resizeEvent = window.document.createEvent('UIEvents');
-  resizeEvent.initUIEvent('resize', true, false, window, 0);
-  window.dispatchEvent(resizeEvent);
-}
-/* eslint-enable */
